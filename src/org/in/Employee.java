e6112ad8f2ef7da24e6691a63c92a7f15dedabe5
Subeshraja37 <subeshraja37@gmail.com>
2023-03-14 01:28:25 +0530

    first

---
+++ src/org/in/Employee.java
@@ -0,0 +1,20 @@
+package org.in;
+
+public class Employee {
+	
+	private void method1() {
+    System.out.println("subesh");
+	}
+	
+	private void method2() {
+	    System.out.println("raja");
+		}
+		
+	private void method3() {
+	    System.out.println("Bharathi");
+		}
+		
+	
+	
+
+}
